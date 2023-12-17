3ec36d2f00d937a6ae9fec4814605f5a19c5c9d3
Mr Bugy <aleksey.serzhuk@mail.ru>
2023-12-17 19:59:29 +0500

    refactoring f18

--- yandex_contest/f18.py
+++ yandex_contest/f18.py
@@ -1,46 +1,24 @@
 import re
 
-pattern_key = re.compile(r'([a-zA-Z][1-9][0-9]*$)|([1-9][0-9]*[a-zA-Z]$)')
-
-
-def check(key):
-    if type(key) is str:
-        if pattern_key.match(key):
-            return
-        else:
-            raise ValueError
-    elif type(key) is tuple:
-        if len(key) != 2:
-            raise TypeError
-        key = str(key[0]) + str(key[1])
-        if pattern_key.match(key):
-            return
-        else:
-            raise ValueError
-    else:
-        raise TypeError
-
-
-def transform(key) -> str:
-    return key.upper() if type(key) is str else (str(key[0])+str(key[1])).upper()
-
 
 class Field(dict):
+    pattern_key = re.compile(r'([a-zA-Z][1-9][0-9]*$)|([1-9][0-9]*[a-zA-Z]$)')
+
     def __getitem__(self, key):
-        check(key)
-        return super(Field, self).__getitem__(frozenset(transform(key)))
+        Field.check(key)
+        return super(Field, self).__getitem__(frozenset(Field.transform(key)))
 
     def __setitem__(self, key, value):
-        check(key)
-        super(Field, self).__setitem__(frozenset(transform(key)), value)
+        Field.check(key)
+        super(Field, self).__setitem__(frozenset(Field.transform(key)), value)
 
     def __delitem__(self, key):
-        check(key)
-        super(Field, self).__delitem__(frozenset(transform(key)))
+        Field.check(key)
+        super(Field, self).__delitem__(frozenset(Field.transform(key)))
 
     def __contains__(self, item):
-        check(item)
-        item = transform(item)
+        Field.check(item)
+        item = Field.transform(item)
         return self[item] != self.__missing__(1)
 
     def __next__(self):
@@ -57,3 +35,22 @@ class Field(dict):
 
     def __missing__(self, key):
         return None
+                
+    @classmethod
+    def check(cls, key) -> None:
+        if not isinstance(key, (str, tuple)):
+            raise TypeError
+        if isinstance(key, str):
+            if cls.pattern_key.match(key) == None:
+                raise ValueError
+        elif isinstance(key, tuple):
+            if len(key) != 2:
+                raise TypeError
+            key = str(key[0]) + str(key[1])
+            if cls.pattern_key.match(key) == None:
+                raise ValueError                
+
+    @staticmethod
+    def transform(key) -> str:
+        return key.upper() if isinstance(key, str) else (str(key[0])+str(key[1])).upper()
+
